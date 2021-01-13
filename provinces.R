a5b9051e66f6ef906e6dee74f3127d54d9785f0f
florisvdh <floris.vanderhaeghe@inbo.be>
2021-01-13 18:31:15 +0100

    provinces.R: autoremoval of trailing spaces

--- provinces.R
+++ provinces.R
@@ -3,13 +3,13 @@ library(dplyr)
 library(httr)
 library(purrr)
 library(ows4R)
-"https://geoservices.informatievlaanderen.be/overdrachtdiensten/VRBG/wfs" %>% 
-    parse_url %>% 
+"https://geoservices.informatievlaanderen.be/overdrachtdiensten/VRBG/wfs" %>%
+    parse_url %>%
     list_merge(query = list(service = "wfs",
                             version = "1.1.0",
                             request = "GetFeature",
                             typeName = "VRBG:Refprv",
                             srsName = "EPSG:31370",
-                            outputFormat = "text/xml; subtype=gml/3.1.1")) %>% 
-    build_url() %>% 
+                            outputFormat = "text/xml; subtype=gml/3.1.1")) %>%
+    build_url() %>%
     read_sf(crs = 31370)
